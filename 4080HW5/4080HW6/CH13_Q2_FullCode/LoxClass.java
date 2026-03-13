12c8555ecb59741168b49e1efacb8ad060f6f0c9
Ali Manasra <167737134+Vex1ng@users.noreply.github.com>
2026-03-12 23:53:26 -0700

    Add files via upload

---
+++ 4080HW5/4080HW6/CH13_Q2_FullCode/LoxClass.java
@@ -0,0 +1,99 @@
+//> Classes lox-class
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import java.util.Map;
+
+/* Classes lox-class < Classes lox-class-callable
+class LoxClass {
+*/
+//> lox-class-callable
+class LoxClass extends LoxInstance implements LoxCallable {
+//< lox-class-callable
+  final String name;
+//> Inheritance lox-class-superclass-field
+  final LoxClass superclass;
+//< Inheritance lox-class-superclass-field
+/* Classes lox-class < Classes lox-class-methods
+
+  LoxClass(String name) {
+    this.name = name;
+  }
+*/
+//> lox-class-methods
+  private final Map<String, LoxFunction> methods;
+  private final Map<String, LoxFunction> getters;
+
+/* Classes lox-class-methods < Inheritance lox-class-constructor
+  LoxClass(String name, Map<String, LoxFunction> methods) {
+*/
+//> Inheritance lox-class-constructor
+LoxClass(String name, LoxClass superclass,
+         Map<String, LoxFunction> methods, LoxClass metaclass,
+         Map<String, LoxFunction> getters) {
+    super(metaclass);
+    this.superclass = superclass;
+//< Inheritance lox-class-constructor
+    this.name = name;
+    this.methods = methods;
+    this.getters = getters;
+  }
+//< lox-class-methods
+//> lox-class-find-method
+LoxFunction findMethod(String name) {
+  if (superclass != null) {
+    LoxFunction method = superclass.findMethod(name);
+    if (method != null) return method;
+  }
+  if (methods.containsKey(name)) return methods.get(name);
+  if (getters.containsKey(name)) return getters.get(name);
+  return null;
+}
+//< lox-class-find-method
+LoxFunction getOwnMethod(String name) {
+  if (methods.containsKey(name)) return methods.get(name);
+  if (getters.containsKey(name)) return getters.get(name);
+  return null;
+}
+
+  LoxClass findMethodOwner(String name) {
+    if (superclass != null) {
+      LoxClass owner = superclass.findMethodOwner(name);
+      if (owner != null) return owner;
+    }
+    if (methods.containsKey(name) || getters.containsKey(name)) return this;
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+//> lox-class-call-arity
+  @Override
+  public Object call(Interpreter interpreter,
+                     List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+//> lox-class-call-initializer
+    LoxFunction initializer = findMethod("init");
+    if (initializer != null) {
+      initializer.bind(instance).call(interpreter, arguments);
+    }
+
+//< lox-class-call-initializer
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+/* Classes lox-class-call-arity < Classes lox-initializer-arity
+    return 0;
+*/
+//> lox-initializer-arity
+    LoxFunction initializer = findMethod("init");
+    if (initializer == null) return 0;
+    return initializer.arity();
+//< lox-initializer-arity
+  }
+//< lox-class-call-arity
+}
